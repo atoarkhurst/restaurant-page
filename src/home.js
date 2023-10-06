9079bf8ae43887c68f3b8f6f7384bd8a4c14fc01
Ato Arkhurst <atoparkhurst@gmail.com>
2023-10-06 06:42:18 -0400

    css styling

--- src/home.js
+++ src/home.js
@@ -9,21 +9,21 @@ const homePageLoad = function () {
 
     const nav = createAndAppend( 'nav', header, { } );
 
-    createAndAppend('div', nav, { innerHTML: 'Home', classList: 'home-tab' } );
+    createAndAppend('button', nav, { innerHTML: 'Home', classList: ['home-tab', 'nav-button']} );
 
-    createAndAppend('div', nav, { innerHTML: 'Menu', classList: 'menu-tab' } );
+    createAndAppend('button', nav, { innerHTML: 'Menu', classList: ['menu-tab' , 'nav-button']} );
 
-    createAndAppend('div', nav, { innerHTML: 'Contact', classList: 'contact-tab' } );
+    createAndAppend('button', nav, { innerHTML: 'Contact', classList: ['contact-tab', 'nav-button'] } );
 
 
     const main = createAndAppend( 'main', content );
-    const welcomeContainer = createAndAppend( 'div', main, { classList: 'welcome-container'})
+    const welcomeContainer = createAndAppend( 'div', main, { classList: ['welcome-container']})
 
     createAndAppend( 'h1', welcomeContainer, { innerHTML: 'Welcome to Bella Cucina Virtuale' } );
 
     createAndAppend( 'p', welcomeContainer, { innerHTML: 'Elevate Your Senses, One Bite at a Time' } );
 
-    createAndAppend( 'button', welcomeContainer, { classList: 'btn', innerHTML: 'View Our Menu' } );
+    createAndAppend( 'button', welcomeContainer, { classList: ['btn'], innerHTML: 'View Our Menu' } );
 
 }
 
@@ -35,7 +35,7 @@ function createAndAppend(tag, parent, attributes = {}) {
             element.innerHTML = attributes[key];
 
         } else if ( key === 'classList') {
-            element.classList.add(attributes[key]);
+            element.classList.add(...attributes[key]);
 
         } else {
             element[key] = attributes[key];
